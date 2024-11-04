133054162f2ebc2715590d32e3c6b60c60108cdb
PROG2-P8 <PROG2-P8-prog2@enetcom.usf.tn>
2024-11-04 16:13:35 +0100

    commit

---
+++ RoomWordSample/app/src/main/java/com/example/roomwordsample/WordViewModel.java
@@ -0,0 +1,28 @@
+package com.example.roomwordsample;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.roomwordsample.data.WordRepository;
+import com.example.roomwordsample.model.Word;
+
+import java.util.List;
+
+public class WordViewModel extends AndroidViewModel {
+
+    private final WordRepository mRepository;
+
+    private final LiveData<List<Word>> mAllWords;
+
+    public WordViewModel (Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() { return mAllWords; }
+
+    public void insert(Word word) { mRepository.insert(word); }
+}
---
+++ RoomWordSample/app/src/main/java/com/example/roomwordsample/data/WordRepository.java
@@ -0,0 +1,35 @@
+package com.example.roomwordsample.data;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.roomwordsample.data.db.WordDao;
+import com.example.roomwordsample.data.db.WordRoomDatabase;
+import com.example.roomwordsample.model.Word;
+
+import java.util.List;
+
+public class WordRepository {
+
+    private final WordDao mWordDao;
+    private final LiveData<List<Word>> mAllWords;
+
+    public WordRepository(Application application) {
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
+        mWordDao = db.wordDao();
+        mAllWords = mWordDao.getAlphabetizedWords();
+    }
+
+    // Room exécute toutes les requêtes sur un thread distinct.
+    // Les données LiveData observées avertiront l'observateur lorsque les données auront changé.
+    public LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    // Vous devez appeler cela sur un thread non-UI ou votre application lancera une exception.
+    // Room garantit que vous n'effectuez aucune opération longue sur le thread principal, bloquant l'interface utilisateur.
+    public void insert(Word word) {
+        WordRoomDatabase.databaseWriteExecutor.execute(() -> mWordDao.insert(word));
+    }
+}
